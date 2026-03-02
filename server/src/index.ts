5ea961502645dc74989bad0439874adf9f4a3d2a
khaira43 <rkhaira1010@gmail.com>
2026-03-01 20:56:01 -0500

    feat: initialize server with MongoDB connection and project config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from "./db";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
